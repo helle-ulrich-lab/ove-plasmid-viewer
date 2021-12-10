977cebb75dbe02fb83aab980464f1678c005e2ed
Nicola Zilio <nicolazilio@gmail.com>
2021-12-10 12:55:01 +0100

    Get plasmid map asynchronously

--- src/App.js
+++ src/App.js
@@ -6,53 +6,60 @@ import { genbankToJson } from 'bio-parsers';
 import "./App.css";
 
 async function AsyncGetGenBankFileAsOveJson(file_name) {
-
-  let data = await fetch(new Request("/uploads/" + file_name, { headers: { "X-Requested-With": "XMLHttpRequest" } }))
-    .then(response => response.text())
-    .then(text => genbankToJson(text)[0]["parsedSequence"])
+  let data = await fetch(
+    new Request("/uploads/" + file_name, {
+      //probably don't need this header.. fetch should just work
+      headers: { "X-Requested-With": "XMLHttpRequest" }
+    })
+  )
+    .then((response) => response.text())
+    .then((text) => genbankToJson(text)[0]["parsedSequence"])
     .catch(console.error);
-
-  return data
-
+  // console.log(`data:`, data); //is this defined and working?
+  return data;
 }
-
-function getGenBankFileAsOveJson(file_name) {
-  var request = new XMLHttpRequest();
-  request.open("GET", "/uploads/" + file_name, false);
-  request.send(null);
-  var json_out = genbankToJson(request.responseText)[0]["parsedSequence"];
-  return json_out;
-}
-
 
 function App() {
-
+  // Get GET parameters from url and store them in a variable
   const search = window.location.search;
   const params = new URLSearchParams(search);
-
-  var plasmid_json_array = getGenBankFileAsOveJson(params.get('file_name'));
-  plasmid_json_array.name = params.get('title');
-  var plasmid_length = plasmid_json_array.size;
-
-  for (let i = 0; i < plasmid_json_array.features.length; i++) {
-    var feat = plasmid_json_array.features[i];
-    var feat_name = feat.name.toLowerCase();
-    if ((feat_name === "synthetic dna construct" || feat_name === "recombinant plasmid") && plasmid_length === feat.end - feat.start + 1) {
-      delete plasmid_json_array.features[i];
-    }
-  }
+  const fileName = params.get("file_name");
+  const title = params.get("title");
 
   React.useEffect(() => {
-    updateEditor(store, "DemoEditor", {
-      sequenceData: plasmid_json_array,
-      circular: plasmid_json_array.circular,
-      annotationVisibility: {
-        features: true,
-        cutsites: false,
-        primers: false
+    //useEffect doesn't like top level async functions so we define one inline and immediately invoke it
+    (async () => {
+      // Get plasmid as OVE JSON
+      const seqData = await AsyncGetGenBankFileAsOveJson(fileName);
+      seqData.name = title;
+      const plasmid_length = seqData.size;
+
+      // Remove features that do not need to be shown, ever!
+      for (let i = 0; i < seqData.features.length; i++) {
+        const feat = seqData.features[i];
+        const feat_name = feat.name.toLowerCase();
+        if (
+          (feat_name === "synthetic dna construct" ||
+            feat_name === "recombinant plasmid") &&
+          plasmid_length === feat.end - feat.start + 1
+        ) {
+          delete seqData.features[i];
+        }
       }
-    });
-  });
+
+      updateEditor(store, "DemoEditor", {
+        sequenceData: seqData,
+        circular: seqData.circular,
+        annotationVisibility: {
+          features: true,
+          cutsites: false,
+          primers: false
+        }
+      });
+      
+    })();
+  }, [title, fileName]);
+
   const editorProps = {
     editorName: "DemoEditor",
     isFullscreen: true,
